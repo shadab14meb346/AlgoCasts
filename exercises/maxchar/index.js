3500013e7016200687a7034f37180a10db271c8d
Shadab <shadab14meb346@gmail.com>
2020-06-07 14:07:38 +0530

    first solution maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function createFreqMap(str) {
+	const freqMap = new Map();
+	for (const char of str) {
+		if (freqMap.has(char)) {
+			const count = freqMap.get(char);
+			freqMap.set(char, count + 1);
+		} else {
+			freqMap.set(char, 1);
+		}
+	}
+	return freqMap;
+}
+function maxChar(str) {
+	const freqMap = createFreqMap(str);
+	const maxFreq = Math.max(...Array.from(freqMap.values()));
+	for (const [char, freq] of freqMap) {
+		if (freq === maxFreq) {
+			return char;
+		}
+	}
+}
 
 module.exports = maxChar;
