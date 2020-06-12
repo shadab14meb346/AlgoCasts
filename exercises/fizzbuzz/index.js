d1a711e2ce1a228d36ea8b3ffec711b04528d194
Shadab <shadab14meb346@gmail.com>
2020-06-12 12:24:04 +0530

    fizzbuzz solution with only if statements

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -11,7 +11,34 @@
 //   fizz
 //   4
 //   buzz
-
-function fizzBuzz(n) {}
+function numberMultipleOfFive(number) {
+  if(number < 5) return false;
+  return !(number % 5)
+}
+function numberMultipleOfThree(number) {
+      if(number < 3) return false;
+  return !(number % 3)
+}
+function numberMultipleOfBothThreeAndFive(number) {
+      if(number < 5 ) return false;
+  return numberMultipleOfFive(number) && numberMultipleOfThree(number)
+}
+function fizzBuzz(n) {
+  for(let i = 1; i <= n; i++) {
+    if(numberMultipleOfThree(i) && !numberMultipleOfBothThreeAndFive(i)) {
+      console.log("fizz");
+      continue;
+    }
+    if(numberMultipleOfFive(i) && !numberMultipleOfBothThreeAndFive(i)) {
+      console.log("buzz");
+      continue;
+    }
+    if(numberMultipleOfBothThreeAndFive(i)) {
+      console.log("fizzbuzz");
+      continue;
+    }
+    console.log(i);
+    }
+}
 
 module.exports = fizzBuzz;
