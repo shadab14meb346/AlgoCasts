b414b505af6e671f1e25fd08b6dad94fc32b57e2
Shadab <shadab14meb346@gmail.com>
2020-07-11 20:18:13 +0530

    recursive solution pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -29,4 +29,20 @@ function pyramid1(n) {
 	}
 }
 
+function pyramid(n, row = 0, level = '') {
+	if (n === row) return;
+	if (level.length === 2 * n - 1) {
+		console.log(level);
+		pyramid(n, row + 1);
+		return;
+	}
+	const midPoint = Math.floor((2 * n - 1) / 2);
+	let add;
+	if (midPoint - row <= level.length && midPoint + row >= level.length) {
+		add = '#';
+	} else {
+		add = ' ';
+	}
+	pyramid(n, row, level + add);
+}
 module.exports = pyramid;
