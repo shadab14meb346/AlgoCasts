25738e3642fe5a13901feacb96839d087908d9b7
Shadab <shadab14meb346@gmail.com>
2020-07-11 20:31:16 +0530

    add solutions for vowel count

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
-
+function vowels1(str) {
+	const vowelRegex = /[a,e,i,o,u,]/;
+	const lowerCaseStr = str.toLowerCase();
+	let vowelCount = 0;
+	for (let char of lowerCaseStr) {
+		if (vowelRegex.test(char)) {
+			vowelCount++;
+		}
+	}
+	return vowelCount;
+}
+function vowels(str) {
+	const matches = str.match(/[aeiou]/gi);
+	return matches ? matches.length : 0;
+}
 module.exports = vowels;
