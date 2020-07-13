e471b36a4b2c83126c4c3f162dccbc6957ee1502
Shadab <shadab14meb346@gmail.com>
2020-07-13 17:35:04 +0530

    add matrix solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function create2dArray(n) {
+	const resultArray = [];
+	for (let i = 0; i < n; i++) {
+		resultArray.push([]);
+	}
+	return resultArray;
+}
+function* range(from = 1, to) {
+	while (from <= to) {
+		yield from++;
+	}
+}
+
+function matrix(n) {
+	let result2dArray = create2dArray(n);
+	let endRow = n - 1;
+	let endColumn = n - 1;
+	let startRow = 0;
+	let startColumn = 0;
+	let counter = 1;
+	while (startColumn <= endColumn && startRow <= endRow) {
+		for (let i = startColumn; i <= endColumn; i++) {
+			result2dArray[startRow][i] = counter;
+			counter++;
+		}
+		startRow++;
+		for (let i = startRow; i <= endRow; i++) {
+			result2dArray[i][endColumn] = counter;
+			counter++;
+		}
+		endColumn--;
+		for (let i = endColumn; i >= startColumn; i--) {
+			result2dArray[endRow][i] = counter;
+			counter++;
+		}
+		endRow--;
+		for (let i = endRow; i >= startRow; i--) {
+			result2dArray[i][startColumn] = counter;
+			counter++;
+		}
+		startColumn++;
+	}
+	return result2dArray;
+}
 
 module.exports = matrix;
