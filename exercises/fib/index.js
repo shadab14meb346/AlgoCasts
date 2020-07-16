68eb014b7cba3e6ca20a7308ce082cb16c603446
Shadab <shadab14meb346@gmail.com>
2020-07-16 17:43:10 +0530

    fib solution iterative

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fibIterative(n) {
+	const result = [0, 1];
+	for (let i = 2; i <= n; i++) {
+		result[i] = result[i - 1] + result[i - 2];
+	}
+	return result[n];
+}
 
+// function fib(n) {
+// 	if (n < 2) return n;
+// 	return fib(n - 1) + fib(n - 2);
+// }
+function memoize(fn) {
+	const cache = {};
+	return function (...args) {
+		if (cache[args]) {
+			return cache[args];
+		}
+		const result = fn.apply(this, args);
+		cache[args] = result;
+		return result;
+	};
+}
+// fib = memoize(fib);
+function fib(n) {
+	if (n < 2) return n;
+	let prev = 0;
+	let next = 1;
+	let current = 0;
+	for (let i = 1; i < n; i++) {
+		current = prev + next;
+		prev = next;
+		next = current;
+	}
+	return current;
+}
 module.exports = fib;
